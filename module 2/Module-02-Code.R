ec4e49f304fec92816542d430ecdd8b74166f7d7
Daniel Maangi <danielmaangi@gmail.com>
2025-01-16 10:29:12 +0300

    updated module 2 code

--- module 2/Module-02-Code.R
+++ module 2/Module-02-Code.R
@@ -239,19 +239,23 @@ print(result)
 # Atomic: Numeric vector
 numeric_vector <- c(1, 2.5, 3.14, 4)
 numeric_vector
+class(numeric_vector)
 
 # Atomic:Character vector
 char_vector <- c("apple", "banana", "cherry")
 char_vector
+class(char_vector)
 
 # Atomic:Logical vector
 logical_vector <- c(TRUE, FALSE, TRUE)
 logical_vector
+class(logical_vector)
 
 
 # List
 my_list <- list(1, "hello", TRUE, c(2, 4, 6))
 my_list
+class(my_list)
 
 
 # ============================================================================
@@ -294,9 +298,9 @@ my_array[1, 2, 1]  # Element at dimension 1: 1, dimension 2: 2, dimension 3: 1
 # most commonly used data structure in R for data analysis.
 
 my_df <- data.frame(
-  name = c("Alice", "Bob", "Charlie"),
-  age = c(25, 30, 28),
-  city = c("New York", "London", "Paris")
+  name = c("Alice", "Bob", "Charlie", NA_character_),
+  age = c(25, 30, 28, NA_integer_),
+  city = c("New York", "London", "Paris", NA_character_)
 )
 
 my_df # Print the data frame
@@ -318,7 +322,7 @@ my_df[, 2]  # Access the second column ("age")
 
 # They are useful for grouping and analyzing data based on categories.
 
-gender <- c("male", "female", "male", "female") # gender vector
+gender <- c("male", "female", "male", "female", "Other") # gender vector
 gender
 gender_factor <- factor(gender) # gender factors
 gender_factor
@@ -338,7 +342,8 @@ vec[c(1, 4)] # first and fourth elements (10, 40)
 vec[2:4]    # the second to the fourth (20, 30, 40)
 
 
-df <- data.frame(name = c("A", "B", "C"), age = c(25, 30, 28))
+df <- data.frame(name = c("A", "B", "C"),
+                 age = c(25, 30, 28))
 df # Print
 df$name     # Access the "name"
 df[, 2]    # Access the second column, age
@@ -359,6 +364,11 @@ vec
 df
 df$age[2] <- 35  # Modify the age of the second row
 df
+
+my_df_copy <- my_df
+
+my_df_copy$age[4] <- 27.66667
+my_df_copy
 
 #======================================================
 # RESOURCES: https://posit.co/resources/cheatsheets/
